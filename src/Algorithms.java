68ed057cf506bb5bbb60934d4483dc9485c2fef4
bikino <bikilde@gmail.com>
2022-08-31 08:28:56 +0200

    First commit

---
+++ src/Algorithms.java
@@ -0,0 +1,35 @@
+public class Algorithms {
+    public static void main(String[] args) {
+
+    }
+    //- Check if the list contains integer X
+    //- FInd duplicate numbers in integer list
+    //- Check if two strings are anagrams
+    //- Remove all duplicates from list
+    //- Find pairs of integers in list so that their sum is equal to integer x
+    //- Checking if a string is a palindrome
+    //- Use list as stack, array and queue
+    //- Get missing number in [1….]]
+    //- Compute the intersection of two lists
+    //- Find max and min in unsorted list
+    //- Reverse string using recursion
+    //- Compute the first Fibonacci numbers
+    //- Sort list with quicksort algorithm
+    //- Find all permutations of string
+    /*
+        You are given a string containing characters  and  only. Your task is to change it into a string such that there are no matching adjacent characters.
+        To do this, you are allowed to delete zero or more characters in the string.
+        Your task is to find the minimum number of required deletions.
+        For example, given the string , remove an  at positions  and  to make  in  deletions.
+        Function Description
+        Complete the alternatingCharacters function in the editor below. It must return an integer representing the minimum number of deletions
+        to make the alternating string.
+        alternatingCharacters has the following parameter(s):
+        s: a string
+        Input Format
+        The first line contains an integer , the number of queries.
+        The next  lines each contain a string .
+     */
+
+
+}
